7377de39d3bf2676dac663d377b61e5c85742859
opsejf <yellowotters1@gmail.com>
2024-02-06 15:08:26 +0000

    safety push pre pull

--- vueproject/src/App.js
+++ vueproject/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from '../src/components/Navbar.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+
+        <Navbar />
+
+
+
+
+
+
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ vueproject/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function(){
+    return (
+
+        <div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>What's On</li>
+                    <li>Trailers</li>
+                    <li>Coming Soon</li>
+                    <li>Big Screen Events</li>
+                    <li>Gift Cards</li>
+                </ul>
+            </nav>
+            <h1>heading</h1>
+        </div>
+    )
+}
