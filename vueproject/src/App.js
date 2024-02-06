a2335078e85c076da117bbc751f839d06718f49c
AGQA2024 <codesoftware2579@gmail.com>
2024-02-06 15:07:14 +0000

    Safety commit pre-pull request.

--- vueproject/src/App.js
+++ vueproject/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles/quickbook.css';
 
 function App() {
   return (
@@ -18,6 +19,11 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div>
+
+      </div>
+        <p className='quickbookbar'>Footer</p>
     </div>
   );
 } 
