fc22266ec15bf07355c7e8cdaf00d4f89e9c1b35
opsejf <yellowotters1@gmail.com>
2024-02-07 16:27:59 +0000

    second version of navbar

--- vueproject/src/components/Navbar.js
+++ vueproject/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../styles/navbar.css";
+import "../styles/navbar1.css";
 import vue from "../Images/vuelogo.jpg";
 import accounticon from "../Images/account.png"
 
@@ -9,21 +9,30 @@ export default function(props){
         <div>
 
             
-            <nav>
+            <nav className="navBar">
+
+                <div className="navLogo">
+                    <img src={vue} id="logo"/>
+                </div>
 
                 <ul className = "navItem">
-                    <li id='firstNav'><img src={vue} id="logo"/></li>
                     <li id="hiddenNav">Home</li>
                     <li id="hiddenNav">What's On</li>
                     <li id="hiddenNav">Trailers</li>
                     <li id="hiddenNav">Coming Soon</li>
                     <li id="hiddenNav">Big Screen Events</li>
                     <li id="hiddenNav" >Gift Cards</li>
+                </ul>
+
+                <ul className="navRight">
                     <li id='lastNav'>Log In</li>
                     <li id="join" >Join</li>
                     <li id="lastLastNav"><img src={accounticon} id="accounticon"/></li>
-
                 </ul>
+
+                    
+
+                
             </nav>
             
         </div>
