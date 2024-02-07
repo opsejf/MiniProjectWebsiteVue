004cb2e5b3e9e3fff07889a0dc98cc8858f71b2e
AGQA2024 <codesoftware2579@gmail.com>
2024-02-07 15:05:14 +0000

    Adding ReactHook window viewsize dynamics.

---
+++ vueproject/src/components/QuickbookSmall.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/quickbook.css';
+
+
+export default function(){
+
+    // Placehold.
+
+    return (
+        <div>
+            <p className='quickbookbar'>Test1</p>
+            <p className='quickbookbar'>Test2</p>
+        </div>
+    )
+};
